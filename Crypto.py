6ceaa49cb33a40e58462fbea27819df66160d8db
rafaelstylianou <117316286+rafaelstylianou@users.noreply.github.com>
2022-11-04 17:51:21 +0200

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -169,8 +169,8 @@ url= 'https://5lagyaru755oxcunku5b3vdiba0krkfd.lambda-url.ap-northeast-1.on.aws/
 response = requests.get(url)
 st.write(response.text)
 
-n3= st.number_input('Insert first number')
-n4= st.number_input('Insert second number')
+n3= st.number_input('Insert third number')
+n4= st.number_input('Insert fourth number')
 
 url= 'https://gk4vntqxykc6abzb6a7tt47nle0ixzsy.lambda-url.ap-northeast-1.on.aws/?num1=%f&num2=%f' % (n3,n4)
 response2 = requests.get(url)
